50b27ef3831e2adba2c4ddd6d8fbe33e96cab513
lucascandidoff <lucascandidoff@gmail.com>
2023-03-29 15:29:36 -0300

    scraping

--- teste10-scraper.py
+++ teste10-scraper.py
@@ -4,8 +4,9 @@ import time
 from utils.lista import lista
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+from lxml import etree
 
-# lista = ['ADOBE SYSTEMS']  
+# lista = ['NGAGE LIVE']  
 
 pbar = tqdm(lista)
 arquivo = open('lista.txt', 'w')
@@ -23,24 +24,41 @@ for tech in pbar:
         if response.status_code == 200: 
             if response.text == '[]':
                 # arquivo.write(f'{tech},null\n')      
-                arquivo.write(f'null,null\n')      
+                arquivo.write(f'null|null\n')      
             else:
+
+                soup = BeautifulSoup(response.content, 'html.parser')
+                texto = soup.currentTag.text.replace('null','None')
+                texto = eval(texto)[0]['html']
+                link = eval(texto.split('href=')[1].split('class')[0].replace('//','https://'))
+               
                 texto = response.text.replace('null','None')
                 html =  eval(texto)[0]['html']
                 html = BeautifulSoup(html, "html.parser")
                 tecnologia = html.text.split('Technology Result:')[1].split('\n')[0]
-                link = eval(texto)[len(eval(texto))-1]['text']
+                # link = eval(texto)[len(eval(texto))-1]['text']
                 if ' · ' in tecnologia:
                     tecnologia = tecnologia.split(' · ')[len(tecnologia.split(' · '))-1]
                 tecnologia = tecnologia.strip().upper()
                 # arquivo.write(f'{tech},{tecnologia}\n')
-                arquivo.write(f'{tecnologia},{link}\n')
+
+                response = requests.get(link)   
+                link_tec = 'null'        
+                if response.status_code == 200: 
+                    soup = BeautifulSoup(response.content, 'html.parser')
+                    link_tec = soup.find_all("div",{"class":"col-9 col-md-10"})[0].contents[3].next.contents[0]
+
+                # response = requests.get(link_tec,headers={'User-Agent': 'Mozilla/5.0'})
+                # if response.status_code == 200: 
+                #     soup = BeautifulSoup(response.content, 'html.parser')
+
+                arquivo.write(f'{tecnologia}|{link_tec}\n')
         else:
             # arquivo.write(f'{tech},null\n')    
-            arquivo.write(f'null,null\n')    
+            arquivo.write(f'null|null\n')    
         # time.sleep(0.3)      
     except Exception as e: 
         # arquivo.write(f'{tech},null\n')    
-        arquivo.write(f'null,null\n')    
+        arquivo.write(f'null|null\n')    
         print(f"Erro: {e}")
         print(f"Tech: {tech}")
